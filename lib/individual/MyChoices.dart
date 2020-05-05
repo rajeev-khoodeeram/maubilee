9abca4217a5b9dfc74efd6780669088f260d09b8
Rajeev Khoodeeram <rajkhoo77@yahoo.com>
2020-05-05 23:48:54 +0400

    Tabs added for Users

--- lib/individual/MyChoices.dart
+++ lib/individual/MyChoices.dart
@@ -13,6 +13,18 @@ class MyChoices extends StatelessWidget {
               subtitle: Text("Buffet ouvert @ 500 / person"),
               leading: Icon(Icons.restaurant),
               trailing: Icon(Icons.arrow_right),
+              ),
+               ListTile(
+              title: Text.rich(TextSpan(text: "l'ocasion en OR !")),
+              subtitle: Text("Honda Fit 2019 @ 300,000..."),
+              leading: Icon(Icons.directions_car),
+              trailing: Icon(Icons.arrow_right),
+              ),
+               ListTile(
+              title: Text.rich(TextSpan(text: "Courts Mammouth")),
+              subtitle: Text("Iphone 21x available now..."),
+              leading: Icon(Icons.mobile_screen_share),
+              trailing: Icon(Icons.arrow_right),
               )
 
           ],
